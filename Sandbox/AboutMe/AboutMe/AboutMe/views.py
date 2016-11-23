0bde69495102d6652c9643f34380321378eb06b9
Gregg <greggburseyjr@gmail.com>
2016-11-23 14:39:45 -0600

    Removed image scraper...I'll do that later...will start on file projects soon

--- Sandbox/AboutMe/AboutMe/AboutMe/views.py
+++ Sandbox/AboutMe/AboutMe/AboutMe/views.py
@@ -9,39 +9,36 @@ from AboutMe import app
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+	"""Renders the home page."""
+	return render_template(
+		'index.html',
+		title='Home Page',
+		year=datetime.now().year,
+	)
 
 @app.route('/contact')
 def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
+	"""Renders the contact page."""
+	return render_template(
+		'contact.html',
+		title='Contact',
+		year=datetime.now().year,
+		message='Your contact page.'
+	)
 
 @app.route('/about')
 def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+	"""Renders the about page."""
+	return render_template(
+		'about.html',
+		title='About',
+		year=datetime.now().year,
+		message='Your application description page.'
+	)
 
-@app.route('/imagescrapper')
-def imagescrapper():
-    """Renders the image scrapper page."""
-    return render_template(
-        'imagescrapper.html',
-        dragonTitle='Image Scrapper',
-        dragonMessage=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/fileprojects')
+def fileprojects():
+	"""Renders file projects page."""
+	return render_template(
+	'fileprojects.html'
+	)
