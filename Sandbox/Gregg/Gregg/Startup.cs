470852e2edef4272144b4de6de4b28fb336da4ec
Gregg <greggburseyjr@gmail.com>
2016-12-07 08:31:36 -0600

    blah!

---
+++ Sandbox/Gregg/Gregg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gregg.Startup))]
+namespace Gregg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
