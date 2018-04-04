6d6fd59debe770fbd3a8bd9f82ecd3dcc05b762f
Gregg Bursey <greggburseyjr@gmail.com>
2018-04-03 20:26:22 -0500

    removed old typescript stuff

--- Udemy/TypeScript/TypeScriptHTMLApp1/TypeScriptHTMLApp1/SCRIPTS/Typescript/math/circle.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PI = 3.14;
-function calcCircumference(diameter) {
-    return diameter * exports.PI;
-}
-exports.calcCircumference = calcCircumference;
--- Udemy/TypeScript/TypeScriptHTMLApp1/TypeScriptHTMLApp1/SCRIPTS/Typescript/math/rectangle.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function calculateRect(width, length) {
-    return width * length;
-}
-exports.default = calculateRect;
