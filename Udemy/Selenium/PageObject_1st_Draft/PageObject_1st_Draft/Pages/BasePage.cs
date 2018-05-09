66334c796459bc0059a3d73bf7644ca1e2b145d7
Gregg Bursey <gbursey@buxtonco.com>
2018-05-09 13:33:09 -0500

    Selenium - fixed issue with base class

--- Udemy/Selenium/PageObject_1st_Draft/PageObject_1st_Draft/Pages/BasePage.cs
+++ Udemy/Selenium/PageObject_1st_Draft/PageObject_1st_Draft/Pages/BasePage.cs
@@ -4,10 +4,11 @@ namespace PageObject_1st_Draft.Pages
 {
     public class BasePage
     {
+        protected IWebDriver Driver { get; set; }
+
         public BasePage(IWebDriver driver)
         {
             Driver = driver;
         }
-        protected IWebDriver Driver { get; set; }
     }
 }
