0a8b4ed72a72302f2bff129a9dd0d067db3ab1f6
Gregg Bursey <gbursey@buxtonco.com>
2018-06-04 15:56:55 -0500

    UDEMY - MVC 5 - more stuff from entity framework and added some model changes

--- Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/Models/Customer.cs
+++ Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/Models/Customer.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        //entity framework recognizes this convention and treats this property as a foriegn key
+        public byte MembershipTypeID { get; set; }
     }
 }
