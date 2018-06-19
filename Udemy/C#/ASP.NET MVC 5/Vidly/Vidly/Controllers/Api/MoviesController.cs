3e33cc17a2cba32ed56f124abd15f2749e52a924
Gregg Bursey <greggburseyjr@gmail.com>
2018-06-19 02:52:59 -0500

    UDEMY - MVC 5 - movies api controller (things may also be broken b/c entity framework errors......may be because I'm doing this from another PC).

---
+++ Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        ////BEFORE DTO
+        //// GET / api/Movies
+        //public IEnumerable<movie> GetMovies()
+        //{
+        //    return _context.Movies.ToList();
+        //}
+        //AFTER DTO
+        // GET / api/Movies
+        [HttpGet]
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList()
+                .Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+        // GET /api/Movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                //throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+                return NotFound();
+
+            //return Mapper.Map<movie, MovieDto>(movie);
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        // POST /api/Movies
+        [HttpPost]
+        //public MovieDto Createmovie(MovieDto MovieDto)
+        public IHttpActionResult CreateMovie(MovieDto MovieDto)
+        {
+            if (!ModelState.IsValid)
+                //throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(MovieDto);
+
+            _context.Movies.Add(movie);//--id will be set from the DB
+            _context.SaveChanges();
+
+            MovieDto.Id = movie.Id;
+
+            //return MovieDto;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), MovieDto);
+        }
+
+        //PUT /api/Movies/1
+        [HttpPut]
+        public void Updatemovie(int id, MovieDto MovieDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(System.Net.HttpStatusCode.BadRequest);
+
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            Mapper.Map(MovieDto, movieInDb);
+
+            _context.SaveChanges();
+        }
+
+        //DELETE /api/Movies/1
+        [HttpDelete]
+        public void Deletemovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+                
+        public byte GenreID { get; set; }
+                
+        public DateTime ReleaseDate { get; set; }
+                
+        public DateTime DateAdded { get; set; }
+                
+        public int NumberInStock { get; set; }
+    }
+}
