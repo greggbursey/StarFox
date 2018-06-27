cd4439d05379ee5e49389f9ee0c4dd78a7324d59
Gregg Bursey <gbursey@buxtonco.com>
2018-06-27 13:58:26 -0500

    UDEMY - MVC 5 - require HTTPS access (on the way to doing OAUTH)

--- Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/App_Start/FilterConfig.cs
+++ Udemy/C#/ASP.NET MVC 5/Vidly/Vidly/App_Start/FilterConfig.cs
@@ -9,6 +9,8 @@ namespace Vidly
             filters.Add(new HandleErrorAttribute());
             //--global application - will apply to all controllers and actions unless [AllowAnonymous] is used
             filters.Add(new AuthorizeAttribute());
+            // end points will no longer be available on an HTTP channel
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
