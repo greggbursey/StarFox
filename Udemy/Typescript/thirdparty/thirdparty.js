0717974a845e35493ac42ba5b47f95cef7987d8e
Gregg Bursey <gbursey@buxtonco.com>
2018-07-05 08:30:11 -0500

    UDEMY - typescript

--- Udemy/Typescript/thirdparty/thirdparty.js
+++ Udemy/Typescript/thirdparty/thirdparty.js
@@ -1,6 +1,8 @@
 "use strict";
 // npm install --save-dev @types/jquery// allows typescript to understand what the $ is
 //npm install -g typings// package manager can handle many 3rd party packages
+Object.defineProperty(exports, "__esModule", { value: true });
+require("jQuery");
 $("#title").css({
     "background-color": "green"
 });
--- Udemy/Typescript/thirdparty/thirdparty.ts
+++ Udemy/Typescript/thirdparty/thirdparty.ts
@@ -1,6 +1,8 @@
 // npm install --save-dev @types/jquery// allows typescript to understand what the $ is
 //npm install -g typings// package manager can handle many 3rd party packages
 
+import "jQuery";
+
 $("#title").css({
     "background-color": "green"
 });
