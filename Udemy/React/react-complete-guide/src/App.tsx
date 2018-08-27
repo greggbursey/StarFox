9633f2beec0af6f412e2d61dd2a0b99b2ea7e4b0
Gregg Bursey <gbursey@buxtonco.com>
2018-08-27 14:05:18 -0500

    UDEMY - react - stuff

--- Udemy/React/react-complete-guide/src/App.tsx
+++ Udemy/React/react-complete-guide/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Person from "./Person/Person";
 
 export default class App extends React.Component {
@@ -21,7 +21,7 @@ export default class App extends React.Component {
     ]
   };
 
-  render() {
+  render(): any {
     return (
       <div className="App">
         <h1>Hi, I'm a ReactApp.</h1>
@@ -29,16 +29,7 @@ export default class App extends React.Component {
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
-        {/* <Person name="Manu" age="29">
-          My Hobbies: racing
-        </Person>
-        <Person name="BLAH" age="0"></Person> */}
       </div>
     );
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   React.createElement("h1", null, "Does this work?")
-    // );
   }
 }
