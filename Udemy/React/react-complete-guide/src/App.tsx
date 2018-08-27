7421b3a9d8036a9a143016d62e9e23bf57036068
Gregg Bursey <gbursey@buxtonco.com>
2018-08-27 13:20:10 -0500

    UDEMY - react - getting into state

--- Udemy/React/react-complete-guide/src/App.tsx
+++ Udemy/React/react-complete-guide/src/App.tsx
@@ -4,15 +4,35 @@ import Person from "./Person/Person";
 
 export default class App extends React.Component {
 
+  public state = {
+    persons: [
+      {
+        name: "Max",
+        age: 28
+      },
+      {
+        name: "Manu",
+        age: 29
+      },
+      {
+        name: "BLAH",
+        age: 0
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a ReactApp.</h1>
-        <Person name="Max" age="28"></Person>
-        <Person name="Manu" age="29">
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        {/* <Person name="Manu" age="29">
           My Hobbies: racing
         </Person>
-        <Person name="BLAH" age="0"></Person>
+        <Person name="BLAH" age="0"></Person> */}
       </div>
     );
     // return React.createElement(
