806ed8b2ff16e5f454bd8ac55117044ba36ebdc0
Gregg Bursey <gbursey@buxtonco.com>
2018-08-27 14:10:05 -0500

    UDEMY - react - event handler

--- Udemy/React/react-complete-guide/src/App.tsx
+++ Udemy/React/react-complete-guide/src/App.tsx
@@ -21,11 +21,15 @@ export default class App extends React.Component {
     ]
   };
 
+  private switchNameHandler = (): void => {
+    alert("BLAH");
+  }
+
   render(): any {
     return (
       <div className="App">
         <h1>Hi, I'm a ReactApp.</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
